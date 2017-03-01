ac1a6c4d8b4cdfe304674b02fb57c3b905c19ce7
Alexander <alexander.pictures@yandex.ru>
2017-03-01 18:03:32 +0300

    функционал общего контроллера распределен по директивам

--- scripts/app.js
+++ scripts/app.js
@@ -5,21 +5,10 @@ angular.module('myApp', [])
         this.list = {
             user: 0
         };
-        this.userDetails = {
-            id: '',
-            name: '',
-            phones: []
-        };
-        this.newCont = {
-            name: '',
-            phones: [{
-                phoneName: 'Мобильный',
-                phoneNumber: ''
-            }]
-        };
         this.showList = [];
+        this.deleteListItem = false;
 
-        function refreshShowListItems() {
+        this.refreshShowListItems = function() {
             this.showList.length = 0;
             for (var key in this.list) {
                 if (key == 'user') continue;
@@ -30,127 +19,162 @@ angular.module('myApp', [])
             }
         };
 
-        function createNewContact() {
-            var id = 'id' + this.list.user;
-            this.list[id] = {};
-            this.list[id].name = this.newCont.name;
-            this.list[id].phones = [];
-            for (var i = 0; i < this.newCont.phones.length; i++) {
-                this.list[id].phones.push({
-                    phoneName: this.newCont.phones[i].phoneName,
-                    phoneNumber: this.newCont.phones[i].phoneNumber
-                });
-            };
-        };
-
-        function clearNewListObject() {
-            this.newCont.name = '';
-            this.newCont.phones.length = 1;
-            this.newCont.phones[0].phoneName = 'Мобильный';
-            this.newCont.phones[0].phoneNumber = '';
-        };
-
         if (localList) {
             this.list = localList;
-            refreshShowListItems.call(this);
+            this.refreshShowListItems();
         }
-
-        this.addNewContact = function() {
-            this.list.user++;
-            createNewContact.call(this);
-            refreshShowListItems.call(this);
-            localStorage.setItem('templateCache', JSON.stringify(this.list));
-            clearNewListObject.call(this);
-        };
-
-        this.addField = function() {
-            this.newCont.phones.push({
-                phoneName: '',
-                phoneNumber: ''
-            });
-        };
-
-        this.removeField = function(index) {
-            this.newCont.phones.splice(index, 1);
-        }
-
-        this.showUserDetails = function(id) {
-            this.userDetails.name = '';
-            this.userDetails.id = '';
-            this.userDetails.phones.length = 0;
-            this.userDetails.id = id;
-            this.userDetails.name = this.list[id].name;
-            for (var i = 0; i < this.list[id].phones.length; i++) {
-                this.userDetails.phones.push({
-                    phoneName: this.list[id].phones[i].phoneName,
-                    phoneNumber: this.list[id].phones[i].phoneNumber
-                });
-            };
-        };
-
-        this.changeUserInfo = function() {
-            var id = this.userDetails.id;
-            this.list[id].name = this.userDetails.name;
-            this.list[id].phones.length = 0;
-            for (var i = 0; i < this.userDetails.phones.length; i++) {
-                this.list[id].phones.push({
-                    phoneName: this.userDetails.phones[i].phoneName,
-                    phoneNumber: this.userDetails.phones[i].phoneNumber
-                });
-            };
-            refreshShowListItems.call(this);
-            localStorage.setItem('templateCache', JSON.stringify(this.list));
-        };
-
-        this.removeUserField = function(index) {
-            this.userDetails.phones.splice(index, 1);
-        };
-
-        this.addUserInfoField = function() {
-            this.userDetails.phones.push({
-                phoneName: '',
-                phoneNumber: ''
-            })
-        };
 
         this.removeListItem = function(item) {
             delete this.list[item];
             localStorage.setItem('templateCache', JSON.stringify(this.list));
-            refreshShowListItems.call(this);
+            this.refreshShowListItems();
         }
     })
-    .directive('firstPage', function(){
-      return {
-        restrict: 'EA',
-        replace: true,
-        templateUrl: 'templates/firstPageTemplate.html'
-      };
+    .directive('firstPage', function() {
+        return {
+            restrict: 'EA',
+            replace: true,
+            scope: {
+                list: '=listAtr'
+            },
+            link: function(scope, element, attrs) {
+                function createNewContact() {
+                    var id = 'id' + scope.list.user;
+                    scope.list[id] = {};
+                    scope.list[id].name = scope.newCont.name;
+                    scope.list[id].phones = [];
+                    for (var i = 0; i < scope.newCont.phones.length; i++) {
+                        scope.list[id].phones.push({
+                            phoneName: scope.newCont.phones[i].phoneName,
+                            phoneNumber: scope.newCont.phones[i].phoneNumber
+                        });
+                    };
+                };
+                function clearNewListObject() {
+                    scope.newCont.name = '';
+                    scope.newCont.phones.length = 1;
+                    scope.newCont.phones[0].phoneName = 'Мобильный';
+                    scope.newCont.phones[0].phoneNumber = '';
+                };
+                scope.newCont = {
+                    name: '',
+                    phones: [{
+                        phoneName: 'Мобильный',
+                        phoneNumber: ''
+                    }]
+                };
+                scope.addNewContact = function() {
+                    scope.list.user++;
+                    createNewContact();
+                    localStorage.setItem('templateCache', JSON.stringify(scope.list));
+                    clearNewListObject();
+                };
+                scope.addField = function() {
+                    scope.newCont.phones.push({
+                        phoneName: '',
+                        phoneNumber: ''
+                    });
+                };
+                scope.removeField = function(index) {
+                    scope.newCont.phones.splice(index, 1);
+                }
+            },
+            templateUrl: 'templates/firstPageTemplate.html'
+        };
     })
-    .directive('secondPage',function(){
-      return {
-        restrict: 'EA',
-        replace: true,
-        templateUrl: 'templates/secondPageTemplate.html'
-      };
+    .directive('secondPage', function() {
+        return {
+            restrict: 'EA',
+            replace: true,
+            templateUrl: 'templates/secondPageTemplate.html'
+        };
     })
-    .directive('listTemplate',function(){
-      return {
-        restrict: 'EA',
-        replace: true,
-        templateUrl: 'templates/listTemplate.html'
-      };
+    .directive('listTemplate', function() {
+        return {
+            restrict: 'EA',
+            replace: true,
+            scope: {
+                showItem: '=showItemAtr',
+                showList: '=showAtr',
+                delete: '=deleteAtr',
+                details: '=detailsAtr'
+            },
+            link: function(scope, element, attrs) {
+                scope.deleteFunc = function(item) {
+                    scope.delete = item;
+                };
+                scope.detailsFunc = function(id) {
+                    scope.details = id;
+                    scope.showItem = 'details';
+                }
+            },
+            templateUrl: 'templates/listTemplate.html'
+        };
     })
-    .directive('changeTemplate',function(){
-      return {
-        restrict: 'EA',
-        replace: true,
-        templateUrl: 'templates/changeTemplate.html'
-      };
+    .directive('detailsTemplate', function() {
+        return {
+            restrict: 'EA',
+            replace: true,
+            scope: {
+                showItem: '=showItemAtr',
+                list: '=listAtr',
+                delete: '=deleteAtr',
+                detailsId: '=detailsAtr'
+            },
+            templateUrl: 'templates/detailsTemplate.html'
+        };
     })
-    .directive('detailsTemplate',function(){
-      return {
-        restrict: 'EA',
-        replace: true,
-        templateUrl: 'templates/detailsTemplate.html'
-      };
+    .directive('changeTemplate', function() {
+        return {
+            restrict: 'EA',
+            replace: true,
+            scope: {
+                showItem: '=showItemAtr',
+                list: '=listAtr',
+                detailsId: '=detailsAtr'
+            },
+            link: function(scope, element, attrs) {
+                scope.contact = {
+                    name: '',
+                    phones: []
+                };
+                scope.update = function() {
+                    scope.contact.name = '';
+                    scope.contact.phones.length = 0;
+                    scope.contact.name = scope.list[scope.detailsId].name;
+                    for (var i = 0; i < scope.list[scope.detailsId].phones.length; i++) {
+                        scope.contact.phones.push({
+                            phoneName: scope.list[scope.detailsId].phones[i].phoneName,
+                            phoneNumber: scope.list[scope.detailsId].phones[i].phoneNumber
+                        });
+                    };
+                };
+                scope.addField = function() {
+                    scope.contact.phones.push({
+                        phoneName: '',
+                        phoneNumber: ''
+                    });
+                };
+
+                scope.removeField = function(index) {
+                    scope.contact.phones.splice(index, 1);
+                };
+                scope.changeUserInfo = function() {
+                    var id = scope.detailsId;
+                    scope.list[id].name = scope.contact.name;
+                    scope.list[id].phones.length = 0;
+                    for (var i = 0; i < scope.contact.phones.length; i++) {
+                        scope.list[id].phones.push({
+                            phoneName: scope.contact.phones[i].phoneName,
+                            phoneNumber: scope.contact.phones[i].phoneNumber
+                        });
+                    };
+                    localStorage.setItem('templateCache', JSON.stringify(scope.list));
+                };
+                scope.$watch('detailsId', function() {
+                    scope.update();
+                });
+            },
+            templateUrl: 'templates/changeTemplate.html'
+        };
     })
